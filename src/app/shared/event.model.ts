733b37113ba6bbb78a682ccd67d29c02655e1ea5
Kumar Abhishek <kumar.abhishek6@cognizant.com>
2018-04-04 20:52:15 +0530

    event model

---
+++ src/app/shared/event.model.ts
@@ -0,0 +1,26 @@
+export interface Events = [
+    {
+      id: 1,
+      name: 'Angular Connect',
+      date: '9/26/2036',
+      time: '10:00 am',
+      price: 599.99,
+      imageUrl: '/app/assets/images/angularconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England'
+      },
+      sessions: [
+        {
+          id: 1,
+          name: "Using Angular 4 Pipes",
+          presenter: "Peter Bacon Darwin",
+          duration: 1,
+          level: "Intermediate",
+          abstract: `Learn all about the new pipes in Angular 4, both 
+          how to write them, and how to get the new AI CLI to write 
+          them for you. Given by the famous PBD, president of Angular 
+          University (formerly Oxford University)`,
+          voters: ['bradgreen', 'igorminar', 'martinfowler']
+        }}]]
